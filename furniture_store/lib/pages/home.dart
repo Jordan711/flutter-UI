a470f37bd959840a9a491f00ac54cf88a3c1b9c7
jordan711 <jordan.feng@hotmail.com>
2025-08-30 00:09:44 -0400

    fix: Apply padding to entire Column instead

--- furniture_store/lib/pages/home.dart
+++ furniture_store/lib/pages/home.dart
@@ -11,6 +11,7 @@ class Home extends StatelessWidget {
       body: SafeArea(
         child: Container(
           width: double.infinity,
+          padding: EdgeInsets.all(32.0),
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("images/table-living-room.png"),
@@ -22,93 +23,71 @@ class Home extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: EdgeInsets.only(left: 32.0, right: 32.0, top: 32.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.add_box_rounded,
-                          color: Colors.white,
-                          size: 40,
-                        ),
-                        Icon(
-                          Icons.ac_unit_rounded,
-                          color: Colors.white,
-                          size: 40,
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Sustainably Crafted,\nBeautifully Designed",
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Curated furniture collections made from sustainable materials by expert craftsmen",
-                      style: TextStyle(fontSize: 12.0, color: Colors.white),
-                    ),
-                  ],
+              Row(
+                children: [
+                  Icon(Icons.add_box_rounded, color: Colors.white, size: 40),
+                  Icon(Icons.ac_unit_rounded, color: Colors.white, size: 40),
+                ],
+              ),
+              Text(
+                "Sustainably Crafted,\nBeautifully Designed",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-
+              Text(
+                "Curated furniture collections made from sustainable materials by expert craftsmen",
+                style: TextStyle(fontSize: 12.0, color: Colors.white),
+              ),
+              
               Spacer(),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(Colors.black),
-                    padding: WidgetStateProperty.all(
-                      EdgeInsets.symmetric(vertical: 20.0),
-                    ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.black),
+                  padding: WidgetStateProperty.all(
+                    EdgeInsets.symmetric(vertical: 20.0),
                   ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (builder) => Catalogue()),
-                    );
-                  },
-                  child: Center(
-                    child: Text(
-                      "Explore Collections",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (builder) => Catalogue()),
+                  );
+                },
+                child: Center(
+                  child: Text(
+                    "Explore Collections",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
 
               SizedBox(height: 12.0),
               Center(
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Already have an account? ",
-                        style: TextStyle(fontSize: 16.0, color: Colors.white),
-                      ),
-                      TextSpan(
-                        text: "Login!",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print('Login tapped!');
-                          },
-                      ),
-                    ],
+                child: RichText(text: TextSpan(children: [
+                  TextSpan(
+                    text: "Already have an account? ",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
+                  TextSpan(
+                    text: "Login!",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      decoration: TextDecoration.underline
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        print('Login tapped!');
+                      }
+                  )
+                ])),
               ),
-
-              SizedBox(height: 30.0),
             ],
           ),
         ),
